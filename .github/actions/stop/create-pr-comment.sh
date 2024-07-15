6f573fe7dd4d0eb60f8aff65a7bb7910845ec6f0
melonattacker <junkitombob@gmail.com>
2024-07-15 23:04:57 +0900

    fix action

--- .github/actions/stop/create-pr-comment.sh
+++ .github/actions/stop/create-pr-comment.sh
@@ -11,7 +11,7 @@ This PR has triggered the signatures:
 
 $(jq .eventName /tmp/tracee-action/signatures.jsonl | sort | uniq )
 
-Please review the Tracee [documentation](https://aquasecurity.github.io/tracee/v0.10/docs/detecting/rules/) to understand the signatures, and review the details of each trigger on the json below:
+Please review the Tracee [documentation](https://aquasecurity.github.io/tracee) to understand the signatures, and review the details of each trigger on the json below:
 
 \`\`\`
 $(cat /tmp/tracee-action/signatures.jsonl)
--- .github/actions/stop/profile/signatures.jq
+++ .github/actions/stop/profile/signatures.jq
@@ -1,2 +1,2 @@
 .[] |  
-select( .eventName == "sched_process_exec" or .eventName == "net_packet_dns" or .eventName == "file_modification" | not )
+select( .eventName == "sched_process_exec" or .eventName == "net_packet_dns_request" or .eventName == "file_modification" | not )
